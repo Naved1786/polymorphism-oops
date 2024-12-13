727b4a45ad38effe21188261729d7eb508a95ac2
Navedul Hasan <navedmalik092@gmail.com>
2024-12-13 19:57:01 +0530

    second commit

--- polymorphism-Naved1786-master/src/main/java/com/driver/Main.java
+++ polymorphism-Naved1786-master/src/main/java/com/driver/Main.java
@@ -1,5 +1,23 @@
 package com.driver;
-
+class Product{
+   public int product(int x,int y){
+        return x*y;
+    }
+    public int product(int x,int y,int z){
+        return x*y*z;
+    }
+  public double product(double x,double y){
+        return x*y;
+    }
+}
 public class Main {
-
+    public static void main(String[] args) {
+        Product p1 = new Product();
+        int result1 = p1.product(3, 5);
+        int result2 = p1.product(3, 2, 4);
+        double result3 = p1.product(3.2, 3.5);
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+    }
 }
